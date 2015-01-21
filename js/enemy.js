f05e1e5e7b6ae95b66928eb44311cfc7edcff182
Sam Blackman <sblackman@gmail.com>
2015-01-21 14:47:15 -0500

    store speed on Enemy model

--- js/enemy.js
+++ js/enemy.js
@@ -4,8 +4,9 @@ function Enemy(options) {
   this.topLeftX = options.topLeftX;
   this.topLeftY = options.topLeftY;
   this.size = options.size;
+  this.speed = options.speed || 15
 }
 
 Enemy.prototype.move = function() {
-  this.topLeftX -= Math.floor(Math.random() * 15) + 5; // TKTKTK: systematize this for different enemy types
+  this.topLeftX -= Math.floor(Math.random() * this.speed) + 5;
 }
