5b22a95d2405caacce79ac9cbb1636a3a1f68f49
Sam Blackman <sblackman@gmail.com>
2015-02-07 14:22:31 -0500

    add methods to Vector model for scaling vectors and summing two vectors

--- js/vector.js
+++ js/vector.js
@@ -3,3 +3,11 @@ function Vector(x, y) {
   // with only one argument passed, that becomes x and y
   arguments.length > 1 ? this.y = y : this.y = x;
 }
+
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+}
+
+Vector.prototype.times = function(factor)  {
+  return new Vector(this.x * factor, this.y * factor);
+}
