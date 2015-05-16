bf0a2dc81d2628e902512818f6c1035c243ac834
Sam Blackman <sblackman@gmail.com>
2015-05-16 16:22:29 -0400

    test Enemy's initialization of HP attribute

--- spec/spec/EnemySpec.js
+++ spec/spec/EnemySpec.js
@@ -11,8 +11,16 @@ describe("Enemy", function() {
     enemy = new Enemy(args);
   })
 
-  // it("has an HP attribute", function() {
-  //   var enemy = new Enemy({maxHp: 100})
-  //   expect(enemy.hp).toEqual(100);
-  // });
+  it("has an HP attribute", function() {
+    expect(enemy.hp).not.toBeUndefined();
+  });
+
+  it("sets its HP attribute to the argument passed to it", function() {
+    expect(enemy.hp).toEqual(startingHp);
+  });
+
+  it("defaults to 100 HP when no HP attribute is passed", function() {
+    var noHpArgEnemy = new Enemy({ topLeftX: 300, topLeftY: 300, target: {center: {x: 100, y: 100}}});
+    expect(noHpArgEnemy.hp).toEqual(100);
+  });
 })
